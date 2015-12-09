f3134f293ea85a143b1716f74c7e29d0e94bbe2e
Paul Sanford <me@paulsanford.net>
2015-12-09 01:03:50 -0500

    Separate project

--- shell/dummy_object.py
+++
@@ -1,7 +0,0 @@
-class DummyObject:
-	def __init__(self):
-		self.name = 'dummy'
-
-	def run(self, command, healthreport):
-		if command == self.name:
-			healthreport['dummy'] = 'Dummy Object'
--- shell/shellcmds.py
+++
@@ -1,3 +0,0 @@
-from dummy_object import DummyObject
-
-command_list = [DummyObject()]
