cc7cebf0cfd718e78f760ce36dc36c349efe6e8b
Paul Sanford <me@paulsanford.net>
2015-12-10 01:47:34 -0500

    Basic site layout

--- status.py
+++ status.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
+import config
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("main.html")
+	print(config.servers)
+	return render_template("main.html", servers=config.servers)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9000)  
