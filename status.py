daa5860c5011c4f580a4e7275d5ce8c0488930a6
Paul Sanford <me@paulsanford.net>
2015-12-10 03:02:46 -0500

    JSON call

--- status.py
+++ status.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 import config
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def index():
 	print(config.servers)
 	return render_template("main.html", servers=config.servers)
+
+@app.route("/stats/<servername>")
+def server_stats(servername):
+	stats = json.dumps([{'name': 'uptime', 'value': '2 days'}])
+	return json_response(stats)
+
+def json_response(response_val):
+	return Response(response=response_val, status=200, mimetype="application/json")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9000)  
